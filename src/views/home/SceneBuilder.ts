dab565c36204df43e3caa632f3b1abe9693eb285
ForEstella <183957330@qq.com>
2024-08-25 22:51:54 +0800

    ✨ feat: 无法加载vrm模型

--- src/views/home/SceneBuilder.ts
+++ src/views/home/SceneBuilder.ts
@@ -1,8 +1,8 @@
 import * as THREE from "three";
-
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { toRaw } from "vue";
 import { AmbientLight, LineBasicMaterial } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { VRMLoaderPlugin, VRMUtils } from "@pixiv/three-vrm";
 
 export default class SceneBuilder {
     root: HTMLElement;
@@ -12,22 +12,29 @@ export default class SceneBuilder {
     controls: OrbitControls;
     material: LineBasicMaterial;
     ambientLight: AmbientLight;
+    light: THREE.DirectionalLight;
 
     constructor(root: HTMLElement, width: number, height: number) {
         this.root = root;
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
-
         this.renderer.setSize(width, height);
         this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.scene.background = new THREE.Color(0x5555555);
+        this.renderer.autoClearColor = true;
+        this.scene.background = new THREE.Color(0xffff);
+        this.light = new THREE.DirectionalLight(0xffffff, Math.PI);
         this.cameraSetup();
-
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.screenSpacePanning = true;
+        this.controls.target.set(0.0, 1.0, 0.0);
+        this.controls.update();
+        this.light.position.set(1.0, 1.0, 1.0).normalize();
+        this.scene.add(this.light);
         this.draw();
         this.root.appendChild(this.renderer.domElement);
     }
 
+    //准备画布
     cameraSetup() {
         this.camera = new THREE.PerspectiveCamera(
             75,
@@ -35,36 +42,50 @@ export default class SceneBuilder {
             0.1,
             1000,
         );
-        this.camera.position.set(-20, 10, 20);
+        this.camera.position.set(0.0, 1.0, 5.0);
         this.camera.lookAt(0, 0, 0);
     }
 
-    draw() {
-        this.material = new THREE.LineBasicMaterial({ color: 0xffffff });
+    async draw() {
+        let currentVrm: { scene: THREE.Object3D<THREE.Event> } | undefined = undefined;
+        const loader = new GLTFLoader();
 
-        const gridHelper = new THREE.GridHelper(500, 50);
+        loader.register((parser) => {
+            return new VRMLoaderPlugin(parser);
+        });
+
+        loader.loadAsync("src/assets/models/nan.vrm").then((gltf) => {
+            const vrm = gltf.userData.vrm;
+
+            // calling these functions greatly improves the performance
+            VRMUtils.removeUnnecessaryVertices(gltf.scene);
+            VRMUtils.removeUnnecessaryJoints(gltf.scene);
+
+            if (currentVrm) {
+                this.scene.remove(currentVrm.scene);
+                VRMUtils.deepDispose(currentVrm.scene);
+            }
+
+            // Disable frustum culling
+            vrm.scene.traverse((obj: { frustumCulled: boolean }) => {
+                obj.frustumCulled = false;
+            });
+
+            currentVrm = vrm;
+            this.scene.add(vrm.scene);
+        });
+
+        // helpers
+        const gridHelper = new THREE.GridHelper(10, 10);
         this.scene.add(gridHelper);
 
         const axesHelper = new THREE.AxesHelper(5);
         this.scene.add(axesHelper);
-
-        const light = new THREE.DirectionalLight(0xffffff, 10);
-        light.position.set(10, 10, 10);
-        this.scene.add(light);
-
-        this.ambientLight = new THREE.AmbientLight(0xaaaaaa);
-        this.scene.add(this.ambientLight);
-
-        const box = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(box, material);
-        cube.position.set(0, -6, -6);
-        this.scene.add(cube);
     }
 
     render = () => {
         requestAnimationFrame(this.render);
-        this.renderer.render(toRaw(this.scene), this.camera);
+        this.renderer.render(this.scene, this.camera);
         this.controls.update();
     };
 }
